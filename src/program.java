1e8af3b2d5a8e4af955ed449a16b2239c77fad95
patryk920 <pblonkowski@wp.pl>
2019-06-03 01:21:39 +0200

    Add new files

---
+++ src/program.java
@@ -0,0 +1,14 @@
+/*Napisz program pobierający trójelementową tablicę liczb i zwracający największą liczbę.
+
+ */
+public class program {
+    public static void main(String[] args) {
+        int[] tablica = new int[]{2000, 700, 10004};
+        if (tablica[0] > tablica[1]&&tablica[0]>tablica[2])
+            System.out.println("Najwieksza liczba to " + tablica[0]);
+        if (tablica[1] > tablica[0]&&tablica[1]>tablica[2])
+            System.out.println("Najwieksza liczba to " + tablica[1]);
+        if (tablica[2] > tablica[0]&&tablica[2]>tablica[1])
+            System.out.println("Najwieksza liczba to " + tablica[2]);
+    }
+}
