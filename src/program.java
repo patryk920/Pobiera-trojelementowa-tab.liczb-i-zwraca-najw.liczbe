7916d7bd93f6306546de7d9816d0b17eab4ac6d0
patryk920 <pblonkowski@wp.pl>
2019-06-11 23:17:04 +0200

    poprawione formatowanie kodu i dodane klamerki do ifow

--- src/program.java
+++ src/program.java
@@ -4,11 +4,14 @@
 public class program {
     public static void main(String[] args) {
         int[] tablica = new int[]{2000, 700, 10004};
-        if (tablica[0] > tablica[1]&&tablica[0]>tablica[2])
+        if (tablica[0] > tablica[1] && tablica[0] > tablica[2]) {
             System.out.println("Najwieksza liczba to " + tablica[0]);
-        if (tablica[1] > tablica[0]&&tablica[1]>tablica[2])
+        }
+        if (tablica[1] > tablica[0] && tablica[1] > tablica[2]) {
             System.out.println("Najwieksza liczba to " + tablica[1]);
-        if (tablica[2] > tablica[0]&&tablica[2]>tablica[1])
+        }
+        if (tablica[2] > tablica[0] && tablica[2] > tablica[1]) {
             System.out.println("Najwieksza liczba to " + tablica[2]);
+        }
     }
 }
